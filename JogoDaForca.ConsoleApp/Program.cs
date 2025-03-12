58ab29cc7accb52117d76dd1bfe51525de38c928
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-03-12 17:18:16 -0300

    Adicionada escolha e exibição de palavra oculta

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -4,20 +4,30 @@
     {
         static void Main(string[] args)
         {
+            string palavraEscolhida = "MELANCIA";
+
+            char[] letrasEncontradas = new char[palavraEscolhida.Length];
+
+            for (int caractere = 0; caractere < letrasEncontradas.Length; caractere++)
+                letrasEncontradas[caractere] = '_';
+
             do
             {
                 Console.Clear();
                 Console.WriteLine("----------------------------------------------");
                 Console.WriteLine("Jogo da Forca");
                 Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("Palavra escolhida: " + String.Join("", letrasEncontradas));
+                Console.WriteLine("----------------------------------------------");
 
                 Console.Write("Digite uma letra: ");
                 char chute = Console.ReadLine()[0];
 
                 Console.WriteLine($"Você digitou: {chute}");
-
                 Console.ReadLine();
             } while (true);
+
+            Console.ReadLine();
         }
     }
 }
