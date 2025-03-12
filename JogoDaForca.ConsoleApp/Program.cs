4c353dcc3bdb78563d1db42ed22ef6cfa8939c08
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-03-12 17:18:59 -0300

    Adicionada validação e mensagem de acerto/erro

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -11,21 +11,58 @@
             for (int caractere = 0; caractere < letrasEncontradas.Length; caractere++)
                 letrasEncontradas[caractere] = '_';
 
+            int quantidadeErros = 0;
+            bool jogadorEnforcou = false;
+            bool jogadorAcertou = false;
+
             do
             {
                 Console.Clear();
                 Console.WriteLine("----------------------------------------------");
                 Console.WriteLine("Jogo da Forca");
                 Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("Erros do jogador: " + quantidadeErros);
+                Console.WriteLine("----------------------------------------------");
                 Console.WriteLine("Palavra escolhida: " + String.Join("", letrasEncontradas));
                 Console.WriteLine("----------------------------------------------");
 
                 Console.Write("Digite uma letra: ");
-                char chute = Console.ReadLine()[0];
+                char chute = Console.ReadLine().ToUpper()[0];
 
-                Console.WriteLine($"Você digitou: {chute}");
-                Console.ReadLine();
-            } while (true);
+                bool letraFoiEncontrada = false;
+
+                for (int contador = 0; contador < palavraEscolhida.Length; contador++)
+                {
+                    char letraAtual = palavraEscolhida[contador];
+
+                    if (chute == letraAtual)
+                    {
+                        letrasEncontradas[contador] = letraAtual;
+                        letraFoiEncontrada = true;
+                    }
+                }
+
+                if (letraFoiEncontrada == false)
+                    quantidadeErros++;
+
+                string palavraEncontrada = String.Join("", letrasEncontradas);
+
+                jogadorAcertou = palavraEncontrada == palavraEscolhida;
+                jogadorEnforcou = quantidadeErros > 5;
+
+                if (jogadorAcertou)
+                {
+                    Console.WriteLine("----------------------------------------------");
+                    Console.WriteLine($"Você acertou a palavra secreta {palavraEscolhida}, parabéns!");
+                    Console.WriteLine("----------------------------------------------");
+                }
+                else if (jogadorEnforcou)
+                {
+                    Console.WriteLine("----------------------------------------------");
+                    Console.WriteLine("Que azar! Tente novamente!");
+                    Console.WriteLine("----------------------------------------------");
+                }
+            } while (jogadorEnforcou == false && jogadorAcertou == false);
 
             Console.ReadLine();
         }
